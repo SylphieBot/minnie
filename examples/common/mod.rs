5900370ba440593dbca5cd20ba04ae92d36e5a97
Lymia Aluysia <lymia@lymiahugs.com>
2020-06-07 16:55:16 -0700

    Add some basic example code.

---
+++ examples/common/mod.rs
@@ -0,0 +1,41 @@
+use std::future::Future;
+use minnie::prelude::*;
+use std::env;
+use std::path::PathBuf;
+use tracing::*;
+use tracing_futures::*;
+use tokio::runtime::Runtime;
+
+fn get_exe_dir() -> PathBuf {
+    let mut path = env::current_exe().expect("cannot get current exe path");
+    path.pop();
+    path
+}
+fn get_root_path() -> PathBuf {
+    match env::var_os("CARGO_MANIFEST_DIR") {
+        Some(manifest_dir) => PathBuf::from(manifest_dir),
+        None => get_exe_dir(),
+    }
+}
+
+pub fn init_tracing() {
+    tracing_log::LogTracer::init().unwrap();
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+}
+pub fn new_context() -> DiscordContext {
+    let mut path = get_root_path().canonicalize().unwrap();
+    path.push("discord_tok");
+    if !path.exists() {
+        panic!("Could not find Discord token at '{}'.", path.display());
+    }
+
+    let tok = std::fs::read_to_string(path).unwrap();
+    DiscordContext::new(DiscordToken::new(&tok).unwrap()).unwrap()
+}
+pub fn start(fut: impl Future<Output = ()> + Send + 'static) {
+    let mut rt = Runtime::new().unwrap();
+    rt.block_on(fut.instrument(info_span!("main_thread")));
+}
---
+++ examples/test_gateway.rs
@@ -0,0 +1,36 @@
+use minnie::gateway::*;
+use minnie::prelude::*;
+use minnie::model::event::*;
+use minnie::model::user::*;
+use tokio::runtime::Handle;
+
+mod common;
+
+struct Dispatch;
+impl GatewayHandler for Dispatch {
+    type Error = minnie::Error;
+
+    fn on_event(
+        &self, ctx: &GatewayContext, ev: GatewayEvent,
+    ) -> Result<(), minnie::Error> {
+        println!("Received packet on shard #{}: {:?}", ctx.shard_id, ev);
+        Ok(())
+    }
+
+    fn on_error(&self, _: &GatewayContext, _: &GatewayError<Self>) -> GatewayResponse {
+        GatewayResponse::Shutdown
+    }
+}
+
+async fn async_main(ctx: DiscordContext) {
+    ctx.gateway().connect(&Handle::current(), Dispatch).await.unwrap();
+    ctx.gateway().set_presence(PresenceUpdate::default().game(Activity::custom_status(None, "Hello, world!")));
+    loop {
+        tokio::time::delay_for(tokio::time::Duration::from_secs(100)).await;
+    }
+}
+
+pub fn main() {
+    common::init_tracing();
+    common::start(async_main(common::new_context()));
+}
